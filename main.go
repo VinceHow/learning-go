96b7687b4070b3bf2f4d5d04d5f417ac60bd410f
Vincent <vincenthao@monzo.com>
2021-09-30 21:53:32 +0100

    Practice var types

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
+
 import "fmt"
-import "rsc.io/quote"
+
+// var types
+//
+//unit	unsigned
+//int		signed
+//byte	uint8
+//float32
+//float64
+//rune	int32, alias
+//bool	T or F
+//strings
+
+// explicit declaration
 
 func main() {
-	fmt.Println(quote.Go())
+	// INITIALISATION
+	var greeting string
+	greeting = "Hello"
+	fmt.Println(greeting)
+	// IMPLICIT DECLARATION, declare and initialise
+	a := 30
+	fmt.Println(a)
+	year := "2021"
+	new_year := "Hi, it's year "
+	var myFloat float32 = 5.5555
+	fmt.Println(new_year + year)
+	fmt.Println(myFloat)
 }
